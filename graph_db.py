f8153063570a4f57dbcd48f138ab77f4edd7382f
Zachary Ives <zives@cis.upenn.edu>
2025-03-28 07:19:13 -0400

    Revamped to just have entities and associations

--- graph_db.py
+++ graph_db.py
@@ -43,20 +43,20 @@ class GraphAccessor:
         """Store a paper by URL and return its paper ID."""
         with self.conn.cursor() as cur:
             # See if paper already exists
-            cur.execute("SELECT id FROM papers WHERE url = %s AND crawl_time = %s;", (url, crawl_time))
+            cur.execute("SELECT entity_id FROM entities WHERE entity_type = 'paper' AND entity_url = %s;", (url, ))
             paper_id = cur.fetchone()
             if paper_id is not None:
                 paper_id = paper_id[0]
                 
-                cur.execute("SELECT tag_value FROM paper_tags WHERE paper_id = %s AND tag_name = %s;", (paper_id, "summary"))
+                cur.execute("SELECT tag_value FROM entity_tags WHERE entity_id = %s AND tag_name = %s;", (paper_id, "summary"))
                 the_tag = cur.fetchone()
                 if the_tag is None:
-                    cur.execute("INSERT INTO paper_tags (paper_id, tag_name, tag_value, tag_embed) VALUES (%s, %s, %s, %s);", (paper_id, "summary", summary, self._generate_embedding(summary)))
+                    cur.execute ("INSERT INTO entity_tags (entity_id, tag_name, tag_value, tag_embed) VALUES (%s, %s, %s, %s);", (paper_id, "summary", summary, self._generate_embedding(summary)))
+            else:            
+                cur.execute("INSERT INTO entities (entity_url, entity_type, entity_name) VALUES (%s, %s, %s) RETURNING entity_id;", (url,'paper',title))
+                paper_id = cur.fetchone()[0]
             
-            cur.execute("INSERT INTO papers (url, crawl_time, title) VALUES (%s, %s, %s) RETURNING id;", (url,crawl_time,title))
-            paper_id = cur.fetchone()[0]
-            
-            cur.execute("INSERT INTO paper_tags (paper_id, tag_name, tag_value, tag_embed) VALUES (%s, %s, %s, %s);", (paper_id, "summary", summary, self._generate_embedding(summary)))
+                cur.execute ("INSERT INTO entity_tags (entity_id, tag_name, tag_value, tag_embed) VALUES (%s, %s, %s, %s);", (paper_id, "summary", summary, self._generate_embedding(summary)))
         self.conn.commit()
         return paper_id
     
@@ -64,22 +64,22 @@ class GraphAccessor:
         """Update all fields of a paper given its ID."""
         with self.conn.cursor() as cur:
             if url is not None:
-                cur.execute("UPDATE papers SET url = %s WHERE id = %s;", (url, paper_id))
+                cur.execute("UPDATE entities SET url = %s WHERE entity_id = %s;", (url, paper_id))
             if crawl_time is not None:
-                cur.execute("UPDATE papers SET crawl_time = %s WHERE id = %s;", (crawl_time, paper_id))
+                cur.execute("UPDATE entities SET crawl_time = %s WHERE entity_id = %s;", (crawl_time, paper_id))
         self.conn.commit()
 
     def fetch_paper_paragraphs(self, paper_id: int) -> List[str]:
         """Fetch a paper's paragraphs by paper ID."""
         with self.conn.cursor() as cur:
-            cur.execute("SELECT content FROM paragraphs WHERE paper_id = %s;", (paper_id,))
+            cur.execute("SELECT entity_detail FROM entities WHERE entity_type = 'paragraph' and entity_parent = %s;", (paper_id,))
             paragraphs = [row[0] for row in cur.fetchall()]
         return paragraphs
 
     def add_source(self, url: str) -> int:
         """Add a source by URL and return its source ID."""
         with self.conn.cursor() as cur:
-            cur.execute("INSERT INTO sources (url) VALUES (%s) RETURNING id;", (url,))
+            cur.execute("INSERT INTO entities (entity_type, entity_url) VALUES (%s,%s) RETURNING entity_id;", ('source', url,))
             source_id = cur.fetchone()[0]
         self.conn.commit()
         return source_id
@@ -87,24 +87,36 @@ class GraphAccessor:
     def link_source_to_paper(self, source_id: int, paper_id: int):
         """Link a source ID to a paper ID."""
         with self.conn.cursor() as cur:
-            cur.execute("INSERT INTO paper_sources (source_id, paper_id) VALUES (%s, %s);", (source_id, paper_id))
+            # Check if the link already exists
+            cur.execute("SELECT 1 FROM entity_link WHERE from_id = %s AND to_id = %s;", (source_id, paper_id))
+            the_link = cur.fetchone()
+            if the_link is None:
+                cur.execute("INSERT INTO entity_link (from_id, to_id, entity_strength, link_type) VALUES (%s, %s, 'source');", (source_id, paper_id,1))
         self.conn.commit()
 
     def link_author_to_paper(self, author_id: int, paper_id: int):
         """Link an author ID to a paper ID."""
         with self.conn.cursor() as cur:
-            cur.execute("INSERT INTO paper_authors (paper_id, author_id) VALUES (%s, %s);", (paper_id, author_id))
+            # Check if the link already exists
+            cur.execute("SELECT 1 FROM entity_link WHERE from_id = %s AND to_id = %s;", (author_id, paper_id))
+            the_link = cur.fetchone()
+            if the_link is None:
+                cur.execute("INSERT INTO entity_link (from_id, to_id, entity_strength, link_type) VALUES (%s, %s, %s, 'author');", (author_id, paper_id, 1))
         self.conn.commit()
 
     def add_paragraph(self, paper_id: int, content: str) -> Tuple[List[float], int]:
         """Add a paragraph, returning an embedding and paragraph ID."""
         embedding = self._generate_embedding(content)
         with self.conn.cursor() as cur:
-            cur.execute(
-                "INSERT INTO paragraphs (paper_id, paragraph_text, embedding) VALUES (%s, %s, %s) RETURNING paragraph_id;",
-                (paper_id, content, embedding)
-            )
-            paragraph_id = cur.fetchone()[0]
+            # Check if the paragraph already exists
+            cur.execute("SELECT entity_id FROM entities WHERE entity_parent = %s AND entity_type = 'paragraph' AND entity_detail = %s;", (paper_id, content,))
+            paragraph_id = cur.fetchone()
+            if paragraph_id is None:
+                cur.execute(
+                    "INSERT INTO entities (entity_parent, entity_type, entity_detail, entity_embed) VALUES (%s, 'paragraph', %s, %s) RETURNING entity_id;",
+                    (paper_id, content, embedding)
+                )
+                paragraph_id = cur.fetchone()[0]
         self.conn.commit()
         return embedding, paragraph_id
     
@@ -112,16 +124,16 @@ class GraphAccessor:
         """Add an author by name and return their author ID."""
         with self.conn.cursor() as cur:
             # Check if the author already exists
-            cur.execute("SELECT author_id FROM authors WHERE author_name = %s;", (name,))
+            cur.execute("SELECT entity_id FROM entities WHERE entity_type = 'author' and entity_name = %s;", (name,))
             author_id = cur.fetchone()
             if author_id is None:
                 # Add the author if they don't exist
-                cur.execute("INSERT INTO authors (author_name) VALUES (%s) RETURNING author_id;", (name,))
+                cur.execute("INSERT INTO entities (entity_type, entity_name) VALUES (%s, %s) RETURNING entity_id;", ('author', name,))
                 author_id = cur.fetchone()[0]
             if email is not None:
-                cur.execute("UPDATE authors SET email = %s WHERE author_id = %s;", (email, author_id))
+                cur.execute("UPDATE entities SET entity_contact = %s WHERE entity_id = %s;", (email, author_id))
             if organization is not None:
-                cur.execute("UPDATE authors SET organization = %s WHERE author_id = %s;", (organization, author_id))
+                cur.execute("UPDATE entities SET entity_detail = %s WHERE entity_id = %s;", (organization, author_id))
         self.conn.commit()
         return author_id
 
@@ -129,9 +141,9 @@ class GraphAccessor:
         """Update all fields of a paragraph given its ID."""
         with self.conn.cursor() as cur:
             if content is not None:
-                cur.execute("UPDATE paragraphs SET paragraph_text = %s WHERE id = %s;", (content, paragraph_id))
+                cur.execute("UPDATE entities SET entity_detail = %s WHERE entity_id = %s;", (content, paragraph_id))
             if embedding is not None:
-                cur.execute("UPDATE paragraphs SET embedding = %s WHERE id = %s;", (embedding, paragraph_id))
+                cur.execute("UPDATE entities SET entity_embed = %s WHERE entity_id = %s;", (embedding, paragraph_id))
         self.conn.commit()
             
     def add_tag_to_paragraph(self, paragraph_id: int, tag: str, tag_value: str):
@@ -151,42 +163,42 @@ class GraphAccessor:
         """Add a tag to a paper."""
         with self.conn.cursor() as cur:
             # Check if the tag already exists
-            cur.execute("SELECT tag_value FROM paper_tags WHERE paper_id = %s AND tag_name = %s;", (paper_id, tag))
+            cur.execute("SELECT tag_value FROM entity_tags WHERE entity_id = %s AND tag_name = %s;", (paper_id, tag))
             the_tag = cur.fetchone()
             if the_tag is None:
-                cur.execute("INSERT INTO paper_tags (paper_id, tag_name, tag_value) VALUES (%s, %s, %s);", (paper_id, tag, tag_value))
+                cur.execute("INSERT INTO entity_tags (entity_id, tag_name, tag_value) VALUES (%s, %s, %s);", (paper_id, tag, tag_value))
             else:
                 # Update the tag if it already exists
-                cur.execute("UPDATE paper_tags SET tag_value = %s WHERE paper_id = %s AND tag_name = %s;", (tag_value, paper_id, tag))
+                cur.execute("UPDATE entity_tags SET tag_value = %s WHERE entity_id = %s AND tag_name = %s;", (tag_value, paper_id, tag))
         self.conn.commit()
 
     def find_paragraphs_by_tag(self, tag: str) -> List[int]:
         """Find all paragraph matches to a tag."""
         with self.conn.cursor() as cur:
-            cur.execute("SELECT paragraph_id FROM paragraph_tags WHERE tag = %s;", (tag,))
+            cur.execute("SELECT entity_id FROM entity_tags WHERE tag_name = %s AND entity_type = 'paragraph';", (tag,))
             paragraph_ids = [row[0] for row in cur.fetchall()]
         return paragraph_ids
 
     def find_tags_for_paragraph(self, paragraph_id: int) -> List[str]:
         """Find all tags for a paragraph."""
         with self.conn.cursor() as cur:
-            cur.execute("SELECT tag FROM paragraph_tags WHERE paragraph_id = %s;", (paragraph_id,))
+            cur.execute("SELECT tag_name FROM entity_tags WHERE entity_id = %s;", (paragraph_id,))
             tags = [row[0] for row in cur.fetchall()]
         return tags
 
-    def find_k_most_similar_paragraphs(self, paragraph_id: int, k: int) -> List[int]:
-        """Find the k most similar paragraphs given a paragraph."""
+    def find_k_most_similar_entities(self, entity_id: int, k: int) -> List[int]:
+        """Find the k most similar entities given an entity."""
         with self.conn.cursor() as cur:
-            cur.execute("SELECT embedding FROM paragraphs WHERE id = %s;", (paragraph_id,))
+            cur.execute("SELECT entity_embed FROM entities WHERE entity_id = %s;", (entity_id,))
             embedding = cur.fetchone()[0]
             cur.execute(
                 """
-                SELECT id FROM paragraphs
-                WHERE id != %s
-                ORDER BY (embedding <-> %s) ASC
+                SELECT id FROM entities
+                WHERE entity_id != %s
+                ORDER BY (entity_embed <-> %s) ASC
                 LIMIT %s;
                 """,
-                (paragraph_id, embedding, k)
+                (entity_id, embedding, k)
             )
             similar_paragraph_ids = [row[0] for row in cur.fetchall()]
         return similar_paragraph_ids
