95f0b3305ffdc28fd684491bccd469129b1d2753
Zachary Ives <zives@cis.upenn.edu>
2025-04-27 10:17:40 -0400

    Further refactor, check duplicate crawl

---
+++ entities/generate_doc_info.py
@@ -0,0 +1,107 @@
+##################
+## PDF document parsing, semantic extraction,
+## and simple enrichment
+##
+## Copyright (C) Zachary G. Ives, 2025
+##################
+
+# Crawls the PDFs listed in the frontier queue
+
+import traceback
+import os
+import json
+import pandas as pd
+import logging
+
+from files.pdfs import split_pdf_with_langchain, get_presplit_aryn_file, chunk_and_partition_pdf_file
+from files.text import index_split_paragraphs
+from crawl.crawler_queue import get_crawled_paths
+
+from files.tables import read_csv, read_json, read_jsonl, read_mat, read_xml, sample_rows_to_string
+from files.tables import create_table_entity
+
+from datetime import datetime
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.schema import Document
+
+from aryn_sdk.partition import partition_file
+# from aryn_sdk.config import ArynConfig
+
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from graph_db import GraphAccessor
+
+from dotenv import load_dotenv, find_dotenv
+
+_ = load_dotenv(find_dotenv())
+
+DOWNLOAD_DIR = os.getenv("PDF_PATH", os.path.expanduser("~/Downloads"))
+
+graph_db = GraphAccessor()
+
+        
+def handle_file(path: str, url: str, use_aryn: bool = False):
+    pdf_path = os.path.join(DOWNLOAD_DIR, path)
+    # Get today's date
+    the_date = datetime.now().date()
+    
+    if path.endswith('.pdf.json'):
+        path = path.replace('.json', '')
+        
+    if path.endswith('.pdf'):
+        if use_aryn:
+            split_docs = chunk_and_partition_pdf_file(pdf_path)
+        else:               
+            # Parse the PDF using Langchain PDF parser
+            pass#split_docs = split_pdf_with_langchain(pdf_path)  # Use the Langchain splitter to split the documents
+    elif path.endswith('.jsonl') or path.endswith('.json') or path.endswith('.csv') or path.endswith('.mat') or path.endswith('.xml'):
+        path = path[7:] # Remove file://
+        if path.endswith('.jsonl'):
+            df = read_jsonl(path)
+        elif path.endswith('.json'):
+            df = read_json(path)
+        elif path.endswith('.csv'):
+            df = read_csv(path)
+        elif path.endswith('.mat'):
+            df = read_mat(path)
+        elif path.endswith('.xml'):
+            df = read_xml(path)
+
+        create_table_entity(path, df, graph_db)        
+    else:
+        logging.info(f"Non-PDF file: {pdf_path}")
+        split_docs = get_presplit_aryn_file(pdf_path)
+    
+        if len(split_docs):
+            index_split_paragraphs(split_docs, url, path, the_date)
+        
+    graph_db.commit()
+    
+def parse_files_and_index(use_aryn: bool = False):
+    """
+    Parse files and index them in the database.  These could be PDFs or tables.
+    This function will check if the document is already indexed in the database.
+    If it is, it will skip the indexing process.
+    If it is not, it will parse the document and index it.
+    This function will also handle any errors that occur during the parsing and indexing process.
+
+    Args:
+        use_aryn (bool, optional): Uses the Aryn parser. Defaults to False.
+    """
+    files = get_crawled_paths()
+
+    for doc in files:
+        doc_id = doc['id']
+        path = doc['path']
+        url = doc['url']
+        try:
+            if not GraphAccessor().exists_document(url):
+                handle_file(path, url, use_aryn)
+            else:
+                logging.info(f"Document {path} is already indexed with ID {doc_id}.")
+
+        except Exception as e:
+            logging.error(f"Error processing PDF {path}: {e}")
+            traceback.print_exc()
+            
---
+++ entities/generate_people_info.py
@@ -0,0 +1,96 @@
+##################
+## Iterate through all authors, organizations, and sources
+## and generate tags describing their credibility
+##
+## Copyright (C) Zachary G. Ives, 2025
+##################
+
+import requests
+from graph_db import GraphAccessor
+
+# Initialize the GraphAccessor
+graph_db = GraphAccessor()
+
+
+from bs4 import BeautifulSoup
+from urllib.parse import unquote
+from langchain_openai import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+from langchain.schema.output_parser import StrOutputParser
+
+from prompts.restructure import truncate_text_to_token_limit
+from prompts.prompt_for_documents import summarize_web_page
+from crawl.semscholar import fetch_author_from_semantic_scholar
+from crawl.duckduckgo import get_author_homepage
+
+# def fix_db():
+#     query = "SELECT entity_id, entity_detail FROM entities WHERE entity_type = 'author' and entity_detail IS NOT NULL and entity_embed IS NULL"
+#     results = graph_db.exec_sql(query)
+#     for author_id, author_detail in results:
+#         print(f"Processing author ID: {author_id}")
+#         if author_detail:
+#             # Generate the embedding for the author detail
+#             embedding = graph_db.generate_embedding(author_detail)
+#             # Update the database with the new embedding
+#             update_query = "UPDATE entities SET entity_embed = %s WHERE entity_id = %s;"
+#             graph_db.execute(update_query, (embedding, author_id))
+#     graph_db.commit()
+
+def get_authors_from_db(with_no_affiliation=False):
+    """
+    Query the list of authors from the entities table.
+    """
+    if with_no_affiliation:
+        query = "SELECT entity_id, entity_name FROM entities WHERE entity_type = 'author' and (entity_detail IS NULL OR length(entity_detail) < 10);"
+    else:
+        query = "SELECT entity_id, entity_name FROM entities WHERE entity_type = 'author';"
+    return graph_db.exec_sql(query)
+
+def update_author_affiliation_in_db(author_id, affiliation):
+    """
+    Update the author's affiliation in the database.
+    """
+    query = "UPDATE entities SET entity_detail = %s, entity_embed = %s WHERE entity_id = %s;"
+    graph_db.execute(query, (affiliation, graph_db.generate_embedding(affiliation), author_id))
+    graph_db.commit()
+
+def process_authors():
+    """
+    Process all authors in the database, fetch their affiliation from Semantic Scholar,
+    and update the database.
+    """
+    authors = get_authors_from_db(True)
+    for author_id, author_name in authors:
+        print(f"Processing author: {author_name} (ID: {author_id})")
+        author_data = fetch_author_from_semantic_scholar(author_name)
+        if author_data and "affiliations" in author_data and author_data["affiliations"]:
+            affiliation = author_data["affiliations"][0]["name"]  # Get the first affiliation
+            print(f"Found affiliation for {author_name}: {affiliation}")
+            update_author_affiliation_in_db(author_id, affiliation)
+        else:
+            print(f"No affiliation found for {author_name}")
+            
+            try:
+                homepage = get_author_homepage(author_name)
+                
+                if homepage:
+                    # Summarize the web page content using LangChain
+                    summary = summarize_web_page(truncate_text_to_token_limit(homepage))
+                    print(f"Summary for {author_name}: {summary}")
+                    
+                    if not "provided HTML" in summary and \
+                    "could not be generated" not in summary:
+                        # Optionally, update the database with the summary
+                        update_author_affiliation_in_db(author_id, summary)
+                    
+                    # break
+
+            except requests.exceptions.RequestException as e:
+                print(f"Error during request: {e}")
+            except Exception as e:
+                print(f"An error occurred: {e}")
+    
+if __name__ == "__main__":
+    # fix_db()
+    process_authors()
+
