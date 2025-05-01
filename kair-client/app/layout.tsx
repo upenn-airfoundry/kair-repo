15f089bbbd3735f6233a465b43b259d9b3a92ac5
Varun Jana <jvarun@seas.upenn.edu>
2025-05-01 12:37:33 -0400

    fix/render hydration error

--- kair-client/app/layout.tsx
+++ kair-client/app/layout.tsx
@@ -26,7 +26,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <AuthProvider>
-        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <body 
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+          suppressHydrationWarning={true}
+        >
           {children}
         </body>
       </AuthProvider>
